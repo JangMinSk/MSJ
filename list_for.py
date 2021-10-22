1762145fc9fbbce1ea0922f0b2134c7e00577c64
장민식 <91999906+JangMinSk@users.noreply.github.com>
2021-10-22 15:56:01 +0900

    Delete list_for.py

--- list_for.py
+++
@@ -1,9 +0,0 @@
-num=[]
-for i in range(10):
-    a=int(input("정수 입력:%d회"%(i+1)))
-    num.append(a)
-    num=list(set(num))
-    print(num)
-    print("----------------------")
-    print(max(num),min(num))      
-            
